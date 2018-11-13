72d605b4df1547417042f56063c6c4fa42ca2736
arifrgilang <rmkspls@gmail.com>
2018-11-13 17:21:54 +0700

    Migrate Util files

---
+++ app/src/main/java/com/rz/rz/footballappfinal/utils/MyDatabaseOpenHelper.kt
@@ -0,0 +1,43 @@
+package com.rz.rz.footballappfinal.utils
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import com.rz.rz.footballappfinal.model.Fav
+import org.jetbrains.anko.db.*
+
+class MyDatabaseOpenHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx,
+    "FavTeam.db", null, 1) {
+    companion object {
+        private var instance: MyDatabaseOpenHelper? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): MyDatabaseOpenHelper {
+            if (instance == null) {
+                instance = MyDatabaseOpenHelper(ctx.applicationContext)
+            }
+            return instance as MyDatabaseOpenHelper
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        // Here you create tables if not exists
+        db.createTable(Fav.TABLE_FAVORITE, true,
+            Fav.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+            Fav.TEAM_ID to TEXT + UNIQUE,
+            Fav.HOME_NAME to TEXT,
+            Fav.AWAY_NAME to TEXT,
+            Fav.HOME_SCORE to TEXT,
+            Fav.AWAY_SCORE to TEXT,
+            Fav.DATE_EVENT to TEXT)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // Here you can upgrade tables, as usual
+        db.dropTable(Fav.TABLE_FAVORITE, true)
+    }
+}
+
+// Access property for Context
+val Context.database: MyDatabaseOpenHelper
+    get() = MyDatabaseOpenHelper.getInstance(applicationContext)
+
